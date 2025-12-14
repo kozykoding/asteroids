e1a60c496d41c14ee623695fe7c63e90d755e3ca
Sammy <sammy@kozykoding.com>
2025-12-13 17:38:34 -0800

    added ticker

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        print(dt)
         screen.fill("black")
         pygame.display.flip()
 
