8216e6fc60f5be372779495360d2ea77e8431a93
Sammy <sammy@kozykoding.com>
2025-12-14 15:00:27 -0800

    added hjkl for directional keys

--- player.py
+++ player.py
@@ -33,13 +33,13 @@ class Player(CircleShape):
         if self.cooldown_timer > 0:
             self.cooldown_timer -= dt
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_h]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_l]:
             self.rotate(+dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_k]:
             self.move(+dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_j]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
